801d9036f7b6320a3bade38466740d28576b9b59
LeeDongHoon <likelion.leedh@gmail.com>
2024-06-30 19:20:24 +0900

    프로젝트 생성

---
+++ arom1/src/main/java/com/example/arom1/Arom1Application.java
@@ -0,0 +1,13 @@
+package com.example.arom1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arom1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arom1Application.class, args);
+	}
+
+}
---
+++ arom1/src/test/java/com/example/arom1/Arom1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arom1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arom1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
