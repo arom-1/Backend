ac55c2fcd632e11f6d5f060014270a65bcd77275
BSHkr <tmdgh784@naver.com>
2024-07-04 19:26:45 +0900

    BoardReply Entity Commit

---
+++ arom1/src/main/java/com/example/arom1/entity/BoardReply.java
@@ -0,0 +1,35 @@
+package com.example.arom1.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "board_reply")
+public class BoardReply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String replyContent;
+
+    private String writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    private BoardReply(String replyContent, String writer) {
+        this.replyContent = replyContent;
+        this.writer = writer;
+    }
+}
