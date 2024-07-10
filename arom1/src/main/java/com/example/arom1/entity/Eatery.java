7c02834615eed0d0f7d0bb30d38ac85bfc384d57
LeeDongHoon <likelion.leedh@gmail.com>
2024-07-11 00:36:40 +0900

    [chore] Eatery 엔티티 수정 식당 정보, 영업 시간 정보 삭제 지번주소, 도로명 주소, 데이터 갱신일자, 업태 구분명 추가 빌더 및 dtoToEntity 정적 메서드 추가

--- arom1/src/main/java/com/example/arom1/entity/Eatery.java
+++ arom1/src/main/java/com/example/arom1/entity/Eatery.java
@@ -1,11 +1,12 @@
 package com.example.arom1.entity;
 
+import com.example.arom1.dto.SeoulEateryDto;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,23 +23,37 @@ public class Eatery extends BaseEntity{
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "address", nullable = false)
-    private String address;
+    //지번주소
+    private String siteWhlAddr;
+
+    //도로명 주소
+    private String rdnWhlAdd;
+
+    //도로명 우편주소
+    private String rdnPostNo;
+
+    //데이터 갱신일자
+    private String updateDt;
+
+    //업태구분명
+    private String uptaeNm;
 
     @Column(name = "telephone", nullable = false)
     private String telephone;
 
-    @Column(name = "information", nullable = false)
-    private String information;
 
     @Column(name = "rating", nullable = true)
     private double rating;
 
-    @Column(name = "opening_hours", nullable = false)
-    private LocalDateTime openingHours;
-
-    @Column(name = "closing_hours", nullable = false)
-    private LocalDateTime closingHours;
+//    @Column(name = "information", nullable = false)
+//    private String information;
+//
+//
+//    @Column(name = "opening_hours", nullable = false)
+//    private LocalDateTime openingHours;
+//
+//    @Column(name = "closing_hours", nullable = false)
+//    private LocalDateTime closingHours;
 
     @OneToMany(mappedBy = "eatery")
     private List<Review> reviews = new ArrayList<>(); //양방향
@@ -50,5 +65,26 @@ public class Eatery extends BaseEntity{
     private List<ChatRoom> chatRooms = new ArrayList<>(); //양방향
 
 
+    @Builder
+    public Eatery(String name, String siteWhlAddr, String rdnWhlAdd, String rdnPostNo, String updateDt, String uptaeNm, String telephone) {
+        this.name = name;
+        this.siteWhlAddr = siteWhlAddr;
+        this.rdnWhlAdd = rdnWhlAdd;
+        this.rdnPostNo = rdnPostNo;
+        this.updateDt = updateDt;
+        this.uptaeNm = uptaeNm;
+        this.telephone = telephone;
+    }
 
+    public static Eatery dtoToEntity(SeoulEateryDto dto){
+        return Eatery.builder()
+                .name(dto.getName())
+                .siteWhlAddr(dto.getSiteWhlAddr())
+                .rdnWhlAdd(dto.getRdnWhlAddr())
+                .rdnPostNo(dto.getRdnPostNo())
+                .updateDt(dto.getUpdateDt())
+                .uptaeNm(dto.getUptaeNm())
+                .telephone(dto.getTelephone())
+                .build();
+    }
 }
