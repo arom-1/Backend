ee4a3bd97b3e217394730036b2c7c4fe84853a6b
BSHkr <tmdgh784@naver.com>
2024-07-11 22:40:31 +0900

    member 컨트롤러 생성

---
+++ arom1/src/main/java/com/example/arom1/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.example.arom1.controller;
+
+import com.example.arom1.dto.MemberDto;
+import com.example.arom1.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public String addMember(MemberDto request) {
+        memberService.save(request);
+
+        return "redirect:/login";
+    }
+}
