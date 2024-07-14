f1a6af92cbb4699ac6b5284da005400659b60a75
BSHkr <tmdgh784@naver.com>
2024-07-14 09:20:15 +0900

    웹시큐리티 기능 추가

--- arom1/src/main/java/com/example/arom1/service/MemberService.java
+++ arom1/src/main/java/com/example/arom1/service/MemberService.java
@@ -4,24 +4,42 @@ import com.example.arom1.dto.MemberDto;
 import com.example.arom1.entity.Member;
 import com.example.arom1.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public Long save(MemberDto dto) {
+
+        PasswordEncoder pw = null;
+
         return memberRepository.save(Member.builder()
                 .email(dto.getEmail())
-                .password(dto.getPassword())
+                .password(passwordEncoder.encode(dto.getPassword()))
                 .name(dto.getName())
                 .introduction(dto.getIntroduction())
                 .build()).getId();
+    }
+
+    @Override
+    public Member loadUserByUsername(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
     }
 }
