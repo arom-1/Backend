00e50d6e2199798d46cc44d618bcda94db8fb043
BSHkr <tmdgh784@naver.com>
2024-07-15 15:51:54 +0900

    null -> ispresent로 수정

--- arom1/src/main/java/com/example/arom1/service/MemberService.java
+++ arom1/src/main/java/com/example/arom1/service/MemberService.java
@@ -1,14 +1,10 @@
 package com.example.arom1.service;
 
-import com.example.arom1.dto.MemberDto;
+import com.example.arom1.common.exception.BaseException;
+import com.example.arom1.common.response.BaseResponseStatus;
 import com.example.arom1.entity.Member;
 import com.example.arom1.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -27,8 +23,8 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         Optional<Member> findMember = memberRepository.findByEmail(member.getEmail());
-        if (findMember != null) {
-            throw new IllegalStateException("이미 가입된 회원입니다.");
+        if (findMember.isPresent()) {
+            throw new BaseException(BaseResponseStatus.EXIST_EMAIL);
         }
     }
 }
