b9ed413935dade67d54cdf8a7f1f2a391fc55852
BSHkr <tmdgh784@naver.com>
2024-07-15 15:52:11 +0900

    Member엔티티 분리해서 구현한거에 따른 코드 수정

--- arom1/src/main/java/com/example/arom1/service/MemberDetailService.java
+++ arom1/src/main/java/com/example/arom1/service/MemberDetailService.java
@@ -1,5 +1,7 @@
 package com.example.arom1.service;
 
+import com.example.arom1.entity.Member;
+import com.example.arom1.entity.MemberDetail;
 import com.example.arom1.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +16,9 @@ public class MemberDetailService implements UserDetailsService {
 
     @Override // 사용자의 이름(email)으로 사용자의 정보를 가져오는 메서드
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return memberRepository.findByEmail(email)
+        Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException((email)));
+
+        return new MemberDetail(member);
     }
 }
