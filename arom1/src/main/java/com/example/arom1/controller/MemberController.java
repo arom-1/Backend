c190a1bfe0d613ea24a0a4a501191e232ff56ae3
BSHkr <tmdgh784@naver.com>
2024-07-15 15:50:31 +0900

    RequestBody 추가

--- arom1/src/main/java/com/example/arom1/controller/MemberController.java
+++ arom1/src/main/java/com/example/arom1/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,7 +22,7 @@ public class MemberController {
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/signup")
-    public String signup(MemberDto request) {
+    public String signup(@RequestBody MemberDto request) {
         Member member = Member.createMember(request, passwordEncoder);
 
         memberService.saveMember(member);
@@ -35,4 +36,5 @@ public class MemberController {
 
         return "redirect:/login";
     }
+
 }
